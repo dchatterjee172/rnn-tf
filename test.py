6a05e91c76da7f1bc4e0c805bd2a173f562d2508
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2017-09-26 11:02:25 +0530

    progress

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ in_w=tf.get_variable("in_w",initializer=tf.random_normal(shape=(1,internal_dim),
 out_w=tf.get_variable("out_w",initializer=tf.random_normal(shape=(internal_dim,1),mean=.5,stddev=1))
 state=tf.nn.elu(tf.matmul(inp,in_w)+tf.matmul(prev_state,prev_w))
 output=tf.nn.elu(tf.matmul(state,out_w))
-loss=tf.reduce_sum(tf.square(output-out))+ploss
+loss=tf.reduce_sum(tf.square(out-output))+ploss
 dout_w=tf.gradients(loss,out_w)
 dprev_w=tf.gradients(loss,prev_w)
 din_w=tf.gradients(loss,in_w)
@@ -26,9 +26,9 @@ tdo=tf.placeholder(name="gradientdout_w",dtype=tf.float32,shape=(internal_dim,1)
 tdh=tf.placeholder(name="gradientdh_w",dtype=tf.float32,shape=(internal_dim,internal_dim))
 tdi=tf.placeholder(name="gradientdin_w",dtype=tf.float32,shape=(1,internal_dim))
 lr=tf.placeholder(name="lrate",dtype=tf.float32,shape=())
-out_w=out_w-tdo*lr
-prev_w=prev_w-tdh*lr
-in_w=in_w-tdi*lr
+change_o=tf.assign(out_w,out_w-tdo*lr)
+change_h=tf.assign(prev_w,prev_w-tdh*lr)
+change_i=tf.assign(in_w,in_w-tdi*lr)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     writer = tf.summary.FileWriter("tfg", sess.graph)
@@ -51,5 +51,5 @@ with tf.Session() as sess:
             tdprev_w+=r[4][0]
             tdin_w+=r[5][0]
         inp_dict={lr:lrate,tdo:tdout_w,tdh:tdprev_w,tdi:tdin_w}
-        x=sess.run([in_w,out_w,prev_w],feed_dict=inp_dict)
+        x=sess.run([change_o,change_i,change_h],feed_dict=inp_dict)
         print(r[0],r[3][0],r[4][0],r[5][0])
